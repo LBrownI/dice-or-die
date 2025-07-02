a473150bd823798787d614b12e210bddaef90864
Lucas Brown <129406202+LBrownI@users.noreply.github.com>
2025-07-02 03:44:26 -0400

    main.yml

---
+++ backend/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testEnvironment: "node", // Specifies that this is a Node.js environment
+  testMatch: ["**/tests/**/*.test.js"], // Looks for test files in a 'tests' folder
+};
